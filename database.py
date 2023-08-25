8e1f8687488b5bf67cd6d273944b350e0a8e2efe
TamaraSavadyan <savadyan.tamara@gmail.com>
2023-08-25 15:30:17 +0300

    added users interaction interface (initial)

--- database.py
+++ database.py
@@ -1,54 +1,61 @@
 import pandas as pd
 from models import Contact
 
+
 class Database:
     db: str = 'database.csv'
 
+
 class CRUD(Database):
-    def create(self, **kwargs):
+    def create(self, **kwargs) -> str:
         df = pd.read_csv(self.db)
         new_row = pd.DataFrame(kwargs, index=[0])
         updated_df = pd.concat([df, new_row], ignore_index=True)
         updated_df.to_csv(self.db, index=False)
         return 'Created successfully'
 
-    def read(self, page=1):
+    def read(self, page: int) -> str:
         records_per_page = 10
         df = pd.read_csv(self.db)
         start_idx = (page - 1) * records_per_page
         end_idx = start_idx + records_per_page
 
         paginated_df = df[start_idx:end_idx]
+        if paginated_df.empty:
+            return 'Page does not exists'
+        
         return paginated_df.to_string(index=False)
 
-    def update(self, id_, **kwargs):
+    def update(self, id_: int, **kwargs) -> str:
         df = pd.read_csv(self.db)
         df.loc[df['id'] == id_, list(kwargs.keys())] = list(kwargs.values())
         df.to_csv(self.db, index=False)
         return 'Updated successfully'
 
-    def delete(self, id_):
+    def delete(self, id_: int) -> str:
         df = pd.read_csv(self.db)
+        entity_to_delete = df[df['id'] == id_]
+
+        if entity_to_delete.empty:
+            return 'Entity was not found'
+        
         df = df[df['id'] != id_]
         df.to_csv(self.db, index=False)
-        return 'Deleted successfully'
-    
-    def filter(self, **kwargs):
+        
+        return f'{entity_to_delete}\nDeleted successfully'
+
+    def filter(self, **kwargs) -> str:
         df = pd.read_csv(self.db)
         filtered_df = df.copy()
 
         for field, value in kwargs.items():
             filtered_df = filtered_df[filtered_df[field] == value]
 
-        if not filtered_df:
-            return 'Not found'
+        if filtered_df.empty:
+            return 'Entity was not found'
 
         return filtered_df.to_string(index=False)
 
 
-
 if __name__ == '__main__':
     crud = CRUD()
-
-
-
--- main.py
+++ main.py
@@ -1,24 +1,47 @@
 from database import CRUD
 
-database_conn = CRUD()
 
 def show_contacts(database_conn: CRUD) -> None:
-    pass
+    page = input('Enter page number you want to see: ')
+    try:
+        int(page)
+    except ValueError:
+        print('You need to provide an integer')
+        return 
+    if not page:
+        page = 1
+    result = database_conn.read(page=int(page))
+    print(result)
+
 
 def update_contact(database_conn: CRUD) -> None:
-    pass
+    id_ = input('Enter contact id to modify it: ')
+
+    result = 5
+
 
 def delete_contact(database_conn: CRUD) -> None:
-    pass
+    id_ = input('Enter contact id to delete it: ')
+    result = database_conn.delete(id_=int(id_))
+    print(result)
 
 def add_contact(database_conn: CRUD) -> None:
     pass
 
+
 def find_contact(database_conn: CRUD) -> None:
     pass
 
+
 def main():
-    pass
+    database_conn = CRUD()
+    print('Welcome to Phone Book !!!\nYou can add new contact to this book\nRead or modify existing contacts')
+    user_input = input('Choose a command. Enter /help to see list of commands\n')
+    if user_input == '/help':
+        print('Congrats u dumb dumb\n')
+    show_contacts(database_conn)
+    delete_contact(database_conn)
+
 
 if __name__ == '__main__':
     main()
